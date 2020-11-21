8949c5534ef4f39e619be332d926d72ef1fe5fc5
murilodpl <inayievmpl@gmail.com>
2020-11-20 21:00:51 -0300

    conexao banco de dados

---
+++ CarhubApp/Conexao.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarhubApp
+{
+    public class Conexao
+    {
+        SqlConnection con = new SqlConnection();
+
+        //Construtor
+        public Conexao()
+        {
+            con.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\iNayi\OneDrive\Documentos\carhub.mdf;Integrated Security=True;Connect Timeout=30";
+
+        }    
+
+        //Metodo Conectar
+        public SqlConnection conectar()
+        {
+            if(con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Open();
+            }
+
+            return con;
+        }
+
+        //Metodo Desconectar
+        public void desconectar()
+        {
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+    }
+}
